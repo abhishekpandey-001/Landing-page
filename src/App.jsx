808e752cf6f822db0ca710a6f90b7ac44595e34b
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-11-23 02:03:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+    <>
+    <header>  
+    <div className="flex justify-between mx-auto bg-[lightseagreen] max-w-[60rem]">
+        <h1><span>B</span>rand</h1>
+        <div>
+            <a href="">Features</a>
+            <a href="">About Us</a>
+            <a href="">Integrations</a>
+            <a href="">Careers</a>
+        </div>
+        <button>Join Waitlist</button>
+    </div>
+</header>
+    </>
+  )
+}
+
+export default App
